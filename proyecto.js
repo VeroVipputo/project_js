60de7296894b2c88d01f178bce0c8e571f0d9651
DiegoTettamanti <tettamanti.diego@gmail.com>
2021-12-04 23:41:08 -0300

    octavo commit

--- proyecto.js
+++ proyecto.js
@@ -4,73 +4,60 @@
     let container;
     let resultado;
     
-const diasVacaciones = () => {
+function diasVacaciones() {
     let container = document.querySelector("#dias");
-    if (antiguedad <= 5) {
-       
+    if ( antiguedad <= 5) {
         console.log("usted tiene 14 dias de vacaciones");
         resultado = salario / 25 * 14;
-        container.outerHTML = ` ( USTED TIENE 14 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 14}) ` ;
-       
-    } else if (antiguedad >=6 & antiguedad <= 10) {
+        container.outerHTML = ` ( USTED TIENE 14 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 14}) `;
+
+    } else if (antiguedad >= 6 & antiguedad <= 10) {
         console.log("Ud tiene 21 días de vacaciones");
         resultado = salario / 25 * 21;
-        container.outerHTML = ` ( USTED TIENE 21 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 21}) ` ;
+        container.outerHTML = ` ( USTED TIENE 21 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 21}) `;
     } else if (antiguedad >= 11 & antiguedad <= 28) {
         console.log("Ud tiene 28 días de vacaciones");
         resultado = salario / 25 * 28;
-        container.outerHTML = ` ( USTED TIENE 28 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 28}) ` ;
+        container.outerHTML = ` ( USTED TIENE 28 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 28}) `;
     } else {
         console.log("usted tiene 35 dias de vacaciones");
         resultado = salario / 25 * 35;
-        container.outerHTML = ` ( USTED TIENE 35 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 35}) ` ;
+        container.outerHTML = ` ( USTED TIENE 35 DIAS DE VACACIONES ) y el Plus vacacional es de $ (${salario / 25 * 35}) `;
     };
-    return antiguedad   
-
-};
-/*plusVacacional();
-//Calculo de plus vacacional
-/* Para calcularlo, se debe dividir el sueldo mensual por 25. Luego, el resultado,
- se debe multiplicar por los días que corresponden de vacaciones.
-    let salario = document.getElementById(parseFloat("salario"));
-    let dias = document.getElementById("dias");
-const plusVacacional = () =>{
-  
-    salario = salario / 25 * dias;
-    return salario
-
-} ;
+    return antiguedad & resultado
+}
 
 
-document.body.onload = addElement;
-
-function addElement () {
-  // crea un nuevo div
-  // y añade contenido
-  var newDiv = document.getElementById("dias");
-  var newContent = document.createTextNode("Tienes"+ dias);
-  newDiv.appendChild(newContent); //añade texto al div creado.
-
-  // añade el elemento creado y su contenido al DOM
-  var currentDiv = document.getElementById("dias");
-  //document.body.addElement(newDiv, currentDiv).innerHtml;
+/* Cálculo Aguinaldo*/
+let trabajados = document.getElementById("trabajados").value;
+let sac = document.getElementById("sac").value;
+ 
+let calculo;
+const Aguinaldo = () => {  
+ 
+    calculo = sac / 12 * trabajados ;
+    let container2 = document.querySelector("#sac2");
+    console.log("Respuesta evento Aguinaldo");
+    console.log(calculo);
+    container2.outerHTML = ` SU CORRESPONDIENTE MEDIO AGUINALDO ES DE $ (${calculo}) `;
+    return calculo
 };
 
+let fiftyex = document.getElementById("fiftyex").value;
+window.addEventListener("keydown", function(e){
+    console.log(e.key);
+})
+let boton50 = document.getElementById("boton50");
+boton50.addEventListener("click", extrasFifth);
+let topay;
+function extrasFifth() {
+     topay = salario / 200 * fiftyex * 1.5;
+     console.log(topay);
+    return topay;
+}
 
-/*
 
-/* Cálculo Aguinaldo
-salario.addEventListener("click", Aguinaldo())
-function Aguinaldo (){
-   
-    console.log("Respuesta evento");
-    
-    
-    return salario 
-      } ;
-Aguinaldo()
-      
-; 
+
 //--------------------------------------------------------------------------------
 /* OBJETOS */
 
@@ -88,7 +75,7 @@ let vacaciones = {
     descuento:   true                   
 }                            
 
-let sac  = {
+let tac  = {
     tipo:        "mejor remuneracion"   ,
     baseCalculo: "ultimo semestre"   ,
     cantidad:    "segun antiguedad"     ,
@@ -108,10 +95,6 @@ let horasExtras100 = {
     baseCalculo: "sueldo basico"        ,
     cantidad:    2                      ,
     descuento:   true                   
-}
-//Ejemplo:
-
-//console.log(horasExtras50);
-//console.log("tipo", horasExtras50.tipo);
-//console.log("El calculo del medio aguinaldo es"+ " "+ sac.tipo +" mensual del "+ sac.baseCalculo + " dividido 2");
-
+};
+/*window.addEventListener("keydown", function(e){
+    console.log(e.key);})*/
