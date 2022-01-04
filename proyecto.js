0c5111255a6e52b6b70864306f405016844b764c
DiegoTettamanti <tettamanti.diego@gmail.com>
2022-01-03 21:40:21 -0300

    commit23

--- proyecto.js
+++ proyecto.js
@@ -121,5 +121,7 @@ $("#mostrar").click(function(){
     extrasFifth.textContent = `$ ${salario / 200 * fiftyex * 1.5}`;
     let payto = document.getElementById("r_4_4");
     payto.textContent = `$ ${salario / 200 * hundredex * 2}`;
+   // let descuentos = document.querySelector("#sub_2");
+    //descuentos.textContent = `$ ${jubilacion - ley19032 - obraSocial - aporteSindical}`;
 
     });
