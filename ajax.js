408a74800ae613c48c542e0f0d6d7972fb6e5348
DiegoTettamanti <tettamanti.diego@gmail.com>
2022-01-06 23:36:15 -0300

    commit28

--- ajax.js
+++ ajax.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
     
-    $.get("https://reqres.in/api/users"),{page: 3}, function(response){
+    $.get("https://reqres.in/api/users"),{page: 2}, function(response){
         response.data.array.forEach(element => {
             $("#datos").append("<p>"+ element.first_name+""+element.last_name + ""+"</p>");
         });
     };
 
-    let usuario = {
-        name: "Veronica Vipputo",
-        mail: "veronicavipputo@gmail.com"
-    }
+    
 
     $.post("https://reqres.in/api/users", nombreUsuario, function(response){
         console.log(response);
+       
     });
 $("#formulario").submit(function(e){
     e.preventDefault();
@@ -22,9 +20,12 @@ $("#formulario").submit(function(e){
         dni: $('input[name="id"]').val(),
      };
      console.log(usuario);
-     $.post($(this).attr("action"), usuario, function(response){
+     $.get($(this).attr("action"), usuario, function(response){
+         response.data.forEach((element, index) => {
          console.log(response);
+         $("#datos").append("<div>"+element.first_name+""+element.last_name+"</div>");
         });
-        return false;
+    });
+       
     });
 });
